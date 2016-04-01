15d3453faa8c8791c87415ad4ef1b9c5d7cc6426
maowerner <s6mawern@uni-bonn.de>
2016-04-01 09:22:16 +0200

    finished rewriting read.py with itertools and read C4+D as well

--- 4pt/boot.py
+++ 4pt/boot.py
@@ -72,20 +72,21 @@ def bootstrap_ensembles(p, nb_boot, bootstrap_original_data):
   ################################################################################
   # read original data and call bootrap procedure
   if bootstrap_original_data:
-    path = './readdata/p%1i/C20_p%1i.npy' % (p, p)
+    diagram = 'C4+B'
+    path = './readdata/p%1i/%s_p%1i.npy' % (p, diagram, p)
     data = np.load(path)
-    path = './readdata/p%1i/C20_p%1i_quantum_numbers.npy' % (p, p)
+    path = './readdata/p%1i/%s_p%1i_quantum_numbers.npy' % (p, diagram, p)
     qn_data = np.load(path)
     if ( (qn_data.shape[0] != data.shape[0])):
       print '\tBootstrapped operators do not aggree with expected operators'
       exit(0)
 
-    binned_data = prebinning(data.real, 50)
+    binned_data = prebinning(data.real, 5)
     print 'Bootstrapping original data for p = %1i. Real part:' % p
-    boot_real = bootstrap(binned_data, 100)
+    boot_real = bootstrap(binned_data, nb_boot)
     print 'Bootstrapping original data. Imaginary part:'
-    binned_data = prebinning(data.imag, 50)
-    boot_imag = bootstrap(binned_data, 100)
+    binned_data = prebinning(data.imag, 5)
+    boot_imag = bootstrap(binned_data, nb_boot)
     print boot_real.shape
     
     if (boot_real.shape[0] != boot_imag.shape[0]):
@@ -95,19 +96,19 @@ def bootstrap_ensembles(p, nb_boot, bootstrap_original_data):
   
   ################################################################################
   # read subduced data and call bootrap procedure
-  path = './readdata/p%1i/C20_p%1i_single_subduced.npy' % (p, p)
-  data = np.load(path)
-  path = './readdata/p%1i/C20_p%1i_single_subduced_quantum_numbers.npy' % (p, p)
-  qn_subduced = np.load(path)
-  if ( (qn_subduced.shape[0] != data.shape[0])):
-    print 'Bootstrapped operators do not aggree with expected operators'
-    exit(0)
-  
-  binned_data = prebinning(data, 50)
-  print 'Bootstrapping subduced data:'
-  boot = bootstrap(binned_data, 100)
-  print boot.shape
-  print '\tfinished bootstrapping subduced operators'
+#  path = './readdata/p%1i/C20_p%1i_single_subduced.npy' % (p, p)
+#  data = np.load(path)
+#  path = './readdata/p%1i/C20_p%1i_single_subduced_quantum_numbers.npy' % (p, p)
+#  qn_subduced = np.load(path)
+#  if ( (qn_subduced.shape[0] != data.shape[0])):
+#    print 'Bootstrapped operators do not aggree with expected operators'
+#    exit(0)
+#  
+#  binned_data = prebinning(data, 50)
+#  print 'Bootstrapping subduced data:'
+#  boot = bootstrap(binned_data, 100)
+#  print boot.shape
+#  print '\tfinished bootstrapping subduced operators'
   
   ################################################################################
   # Write data to disc ###########################################################
@@ -123,11 +124,11 @@ def bootstrap_ensembles(p, nb_boot, bootstrap_original_data):
   
   if bootstrap_original_data:
     # write all operators
-    path = './bootdata/p%1i/C20_p%1i_real' % (p, p)
+    path = './bootdata/p%1i/%s_p%1i_real' % (p, diagram, p)
     np.save(path, boot_real)
-    path = './bootdata/p%1i/C20_p%1i_imag' % (p, p)
+    path = './bootdata/p%1i/%s_p%1i_imag' % (p, diagram, p)
     np.save(path, boot_imag)
-    path = './bootdata/p%1i/C20_p%1i_quantum_numbers' % (p, p)
+    path = './bootdata/p%1i/%s_p%1i_quantum_numbers' % (p, diagram, p)
     np.save(path, qn_data)
   
     # write every operator seperately
@@ -152,56 +153,56 @@ def bootstrap_ensembles(p, nb_boot, bootstrap_original_data):
   ################################################################################
   # write all subduced correlators
   
-  path = './bootdata/p%1i/C20_p%1i_single_subduced' % (p, p)
-  np.save(path, boot)
-  path = './bootdata/p%1i/C20_p%1i_single_subduced_quantum_numbers' % (p, p)
-  np.save(path, qn_subduced)
-   
-  # write means over all operators subducing into same irrep
-  if p not in [1,3,4]:
-    path = './bootdata/p%1i/C20_p%1i_avg_subduced' % (p, p)
-    np.save(path, np.mean(boot, axis=2) )
-    path = './bootdata/p%1i/C20_p%1i_avg_subduced_quantum_numbers' % (p, p)
-    np.save(path, qn_subduced[...,0,-3:])
-  else:
-    # if p in [1,3,4], the 2dim E2 irrep appears. Both rows can be averaged, thus
-    # the average is taken and appended in the end for plotting
-    path = './bootdata/p%1i/C20_p%1i_avg_subduced' % (p, p)
-    E2 = np.zeros((1,) + np.mean(boot, axis=2)[0].shape)
-    for j in range(0,boot.shape[1]):
-      E2[0,j] = np.mean(np.vstack((boot[1,j], boot[2,j])), axis=0)
-    np.save(path, np.vstack((np.mean(boot, axis=2), E2 )) )
-    path = './bootdata/p%1i/C20_p%1i_avg_subduced_quantum_numbers' % (p, p)
-    E2_qn = np.zeros((1,qn_subduced.shape[1], qn_subduced[...,-3:].shape[-1]), \
-                                                                  dtype=((str,256)))
-    E2_qn[0] = qn_subduced[0,:,0,-3:]
-    for j in range(0,boot.shape[1]):
-      E2_qn[0,j,-1] = 'E2'
-    np.save(path, np.vstack((qn_subduced[...,0,-3:], E2_qn)) )
-  
-  ################################################################################
-  # write the subduced correlators for each irrep and gamma seperately
-  for i in ( range(0, boot.shape[0]) ):
-    for j in ( range(0, boot.shape[1]) ):
-      path = './bootdata/p%1i/single/C20_p%1i_single_%s_%s_%s' % (p, p, \
-              qn_subduced[i,j,0,-3], qn_subduced[i,j,0,-2], qn_subduced[i,j,0,-1])
-      np.save(path, boot[i,j])
-      path = './bootdata/p%1i/single/C20_p%1i_single_%s_%s_%s_quantum_numbers' % \
-              (p, p, qn_subduced[i,j,0,-3], qn_subduced[i,j,0,-2], 
-                                                            qn_subduced[i,j,0,-1])
-      np.save(path, qn_subduced[i,j])
-  
-  # write means over all operators subducing into same irrep for each irrep and 
-  # gamma seperately
-  for i in ( range(0, boot.shape[0]) ):
-    for j in ( range(0, boot.shape[1]) ):
-      path = './bootdata/p%1i/avg/C20_p%1i_avg_%s_%s_%s' % (p, p, \
-              qn_subduced[i,j,0,-3], qn_subduced[i,j,0,-2], qn_subduced[i,j,0,-1])
-      np.save(path, np.mean(boot[i,j], axis=0) )
-      path = './bootdata/p%1i/avg/C20_p%1i_avg_%s_%s_%s_quantum_numbers' % (p, p, \
-              qn_subduced[i,j,0,-3], qn_subduced[i,j,0,-2], qn_subduced[i,j,0,-1])
-      np.save(path, qn_subduced[i,j,0,-1])
-  
-  print '\tfinished writing'
+#  path = './bootdata/p%1i/C20_p%1i_single_subduced' % (p, p)
+#  np.save(path, boot)
+#  path = './bootdata/p%1i/C20_p%1i_single_subduced_quantum_numbers' % (p, p)
+#  np.save(path, qn_subduced)
+#   
+#  # write means over all operators subducing into same irrep
+#  if p not in [1,3,4]:
+#    path = './bootdata/p%1i/C20_p%1i_avg_subduced' % (p, p)
+#    np.save(path, np.mean(boot, axis=2) )
+#    path = './bootdata/p%1i/C20_p%1i_avg_subduced_quantum_numbers' % (p, p)
+#    np.save(path, qn_subduced[...,0,-3:])
+#  else:
+#    # if p in [1,3,4], the 2dim E2 irrep appears. Both rows can be averaged, thus
+#    # the average is taken and appended in the end for plotting
+#    path = './bootdata/p%1i/C20_p%1i_avg_subduced' % (p, p)
+#    E2 = np.zeros((1,) + np.mean(boot, axis=2)[0].shape)
+#    for j in range(0,boot.shape[1]):
+#      E2[0,j] = np.mean(np.vstack((boot[1,j], boot[2,j])), axis=0)
+#    np.save(path, np.vstack((np.mean(boot, axis=2), E2 )) )
+#    path = './bootdata/p%1i/C20_p%1i_avg_subduced_quantum_numbers' % (p, p)
+#    E2_qn = np.zeros((1,qn_subduced.shape[1], qn_subduced[...,-3:].shape[-1]), \
+#                                                                  dtype=((str,256)))
+#    E2_qn[0] = qn_subduced[0,:,0,-3:]
+#    for j in range(0,boot.shape[1]):
+#      E2_qn[0,j,-1] = 'E2'
+#    np.save(path, np.vstack((qn_subduced[...,0,-3:], E2_qn)) )
+#  
+#  ################################################################################
+#  # write the subduced correlators for each irrep and gamma seperately
+#  for i in ( range(0, boot.shape[0]) ):
+#    for j in ( range(0, boot.shape[1]) ):
+#      path = './bootdata/p%1i/single/C20_p%1i_single_%s_%s_%s' % (p, p, \
+#              qn_subduced[i,j,0,-3], qn_subduced[i,j,0,-2], qn_subduced[i,j,0,-1])
+#      np.save(path, boot[i,j])
+#      path = './bootdata/p%1i/single/C20_p%1i_single_%s_%s_%s_quantum_numbers' % \
+#              (p, p, qn_subduced[i,j,0,-3], qn_subduced[i,j,0,-2], 
+#                                                            qn_subduced[i,j,0,-1])
+#      np.save(path, qn_subduced[i,j])
+#  
+#  # write means over all operators subducing into same irrep for each irrep and 
+#  # gamma seperately
+#  for i in ( range(0, boot.shape[0]) ):
+#    for j in ( range(0, boot.shape[1]) ):
+#      path = './bootdata/p%1i/avg/C20_p%1i_avg_%s_%s_%s' % (p, p, \
+#              qn_subduced[i,j,0,-3], qn_subduced[i,j,0,-2], qn_subduced[i,j,0,-1])
+#      np.save(path, np.mean(boot[i,j], axis=0) )
+#      path = './bootdata/p%1i/avg/C20_p%1i_avg_%s_%s_%s_quantum_numbers' % (p, p, \
+#              qn_subduced[i,j,0,-3], qn_subduced[i,j,0,-2], qn_subduced[i,j,0,-1])
+#      np.save(path, qn_subduced[i,j,0,-1])
+#  
+#  print '\tfinished writing'
 
-bootstrap_ensembles(2, 2000, True)
+bootstrap_ensembles(2, 500, True)
--- 4pt/read.py
+++ 4pt/read.py
@@ -21,14 +21,12 @@ T = 48
 p_cm = 0
 p_max = 4
 
-p_cm_max = np.asarray((6,5,6,7,4), dtype=int)
-lookup_p3_reduced = np.asarray((np.asarray((0,0,0), dtype=int), \
-                           np.asarray((0,0,1), dtype=int), \
-                           np.asarray((0,1,1), dtype=int), \
-                           np.asarray((1,1,1), dtype=int), \
-                           np.asarray((0,0,2), dtype=int) ))
+p_cm_max = np.asarray((4,5,6,7,4), dtype=int)
+lookup_p3_reduced = [(0,0,0), (0,0,1), (0,1,1), (1,1,1), (0,0,2)]
 
-directory = '/hiskp2/knippsch/Rho_Feb2016/'
+diagrams = ['C4+B', 'C4+D']
+
+directory = ['/hiskp2/knippsch/Rho_Feb2016/', '/hiskp2/knippsch/Rho_Jan2016/']
 
 missing_configs = [1282]
 
@@ -44,185 +42,181 @@ def abs2(x):
 
 #TODO: create np-array with all necessary momenta in z-direction -> dudek paper
 
-count = 0
-for p_cm in range(0, 1):
+for p_cm in range(2, 3):
   print 'p_cm = %i' % p_cm
+  for d, diagram in enumerate(diagrams):
+    print '\tread diagram %s' % diagram
 
-  # create lookup table for all possible 3-momenta that can appear in our 
-  # contractions
-  b = it.ifilter(lambda x: abs2(x) <= p_max, \
-                              it.product(range(-p_max, p_max+1), repeat=3))
+    # create lookup table for all possible 3-momenta that can appear in our 
+    # contractions
+    lookup_p3 = it.ifilter(lambda x: abs2(x) <= p_max, \
+                                   it.product(range(-p_max, p_max+1), repeat=3))
+   
+    # create lookup table with all possible 3-momentum combinations that 
+    # generate 4pt functions with the correct center-of-mass momentum and 
+    # respect momentum conservation
+    if diagram == 'C4+B':
+      lookup_p = it.ifilter(lambda (w,x,y,z): \
+                 # if the sum of both momenta is the desired center of mass 
+                 # momentum
+                 tuple(it.imap(operator.add, w, z)) == lookup_p3_reduced[p_cm] \
+                 and tuple(it.imap(operator.neg, it.imap(operator.add, x, y))) \
+                                                    == lookup_p3_reduced[p_cm] \
+                 # for zero center of mass momentum omit the case were both 
+                 # particles are at rest (s-wave)
+                 and not (p_cm == 0 and 
+                              (tuple(w) == tuple(z) or tuple(x) == tuple(y) )) \
+                 # omit cases for which both particles carry high momentum 
+                 # cancelling to the desired cm-momentum
+                 and (abs2(w) + abs2(z) <= p_cm_max[p_cm]) and \
+                                        (abs2(x) + abs2(y) <= p_cm_max[p_cm]), \
+                          # create lookup table with all possible combinations 
+                          # of four 3-momenta 
+                          it.product(lookup_p3, repeat=4))
+
+#      # box diagram
+#      lookup_p_b = []
+#      for p_so in lookup_p_reduced:
+#        for p_si in lookup_p_reduced:
+#          if ( (p_cm != 0) or (np.dot(p_so[0], p_so[0]) < 3 and np.dot(p_si[0], p_si[0]) < 3) ):
+#            lookup_p_b.append(np.vstack((p_so[0], -p_si[0], -p_si[1], p_so[1])) )
  
-  # create lookup table for all possible sums of 3-momenta that give the right 
-  # center-of-mass momentum 
-  d = it.product(b, repeat=2)
-  e = filter(lambda x: abs2(x[0]) + abs2(x[1]) <= p_cm_max[p_cm], d)
-  lookup_p = it.ifilter(lambda x: abs2(list(it.imap(operator.add, x[0], x[1]))) == p_cm, e)
-
-  lookup_p = np.asarray(list(lookup_p))
-  print lookup_p.shape
-
-  #TODO: change that to tuples
-  lookup_p_reduced = it.ifilter( \
-      lambda x: np.array_equal(np.asarray(list(it.imap(operator.add, x[0], x[1]))), \
-                               lookup_p3_reduced[p_cm]) \
-                and (abs2(list(it.imap(operator.add, x[0], x[1]))) != 0 or \
-                not np.array_equal(np.asarray(x[0]), np.asarray(x[1]))), e)
-
-  lookup_p_reduced = np.asarray(list(lookup_p_reduced))
-  print lookup_p_reduced.shape
-
-  # create lookup table with all possible 3-momentum combinations that generate
-  # 4pt functions with the correct center-of-mass momentum and respect momentum
-  # conservation
-
-  # direct diagram
-  lookup_p_d = []
-  for p_so in lookup_p:
-    for p_si in lookup_p:
-      if (np.array_equal(p_so[0]+p_so[1], p_si[0]+p_si[1])):
-        lookup_p_d.append(np.vstack((p_so[0], -p_si[0], p_so[1], -p_si[1])) )
-
-  # box diagram
-  lookup_p_b = []
-  for p_so in lookup_p_reduced:
-    for p_si in lookup_p_reduced:
-#      if p_cm != 0 or (np.dot(p_so[0],p_so[0]) ==  np.dot(p_si[0],p_si[0])):
-      if ( (p_cm != 0) or (np.dot(p_so[0], p_so[0]) < 3 and np.dot(p_si[0], p_si[0]) < 3) ):
-        lookup_p_b.append(np.vstack((p_so[0], -p_si[0], -p_si[1], p_so[1])) )
-
-  lookup_p_b = np.asarray(lookup_p_b)
-  lookup_p_d = np.asarray(lookup_p_d)
+    elif diagram == 'C4+D':
+      lookup_p = it.ifilter(lambda (w,x,y,z): \
+                 # if the sum of both momenta is the desired center of mass 
+                 # momentum
+                 tuple(it.imap(operator.add, w, y)) == lookup_p3_reduced[p_cm] \
+                 and tuple(it.imap(operator.neg, it.imap(operator.add, x, z))) \
+                                                    == lookup_p3_reduced[p_cm] \
+                 # for zero center of mass momentum omit the case were both 
+                 # particles are at rest (s-wave)
+                 and not (p_cm == 0 and 
+                              (tuple(w) == tuple(y) or tuple(x) == tuple(z) )) \
+                 # omit cases for which both particles carry high momentum 
+                 # cancelling to the desired cm-momentum
+                 and (abs2(w) + abs2(y) <= p_cm_max[p_cm]) and \
+                                        (abs2(x) + abs2(z) <= p_cm_max[p_cm]), \
+                          # create lookup table with all possible combinations 
+                          # of four 3-momenta 
+                          it.product(lookup_p3, repeat=4))
+#      #direct diagram
+#      lookup_p_d = []
+#      for p_so in lookup_p:
+#        for p_si in lookup_p:
+#          if (np.array_equal(p_so[0]+p_so[1], p_si[0]+p_si[1])):
+#            lookup_p_d.append(np.vstack((p_so[0], -p_si[0], p_so[1], -p_si[1])) )
   
-  if verbose:
-    print lookup_p_d.shape
-    print lookup_p_b.shape
-    print ' '
-  
-#for p_test in lookup_p3:
-#  counter = 0
-#  for i in range(lookup_p_b.shape[0]):
-#    if np.array_equal(lookup_p_b[i, 0], p_test):
-##      print lookup_p_b[i]
-##      print ' '
-#      counter = counter + 1
-#  print p_test
-#  print counter
-
-#for i in range(lookup_p_b.shape[0]):
-#  if np.array_equal(lookup_p_b[i, 0], np.asarray((0,-1,0))):
-##    if np.array_equal(lookup_p_b[i, 1], np.asarray((0,-1,0))):
-#    print lookup_p_b[i]
-#    print ' '
-
-  # calculate number of configurations
-  nb_cnfg = 0
-  for i in range(sta_cnfg, end_cnfg+1, del_cnfg):
-    if i in missing_configs:
-      continue
-    nb_cnfg = nb_cnfg + 1
-  if(verbose):
-    print 'number of configurations: %i' % nb_cnfg
-
-  #TODO: check if all files are there
-  data = [None]*nb_cnfg
-  cnfg = 0
-
-  for i in range(sta_cnfg, end_cnfg+1, del_cnfg):
-    if i in missing_configs:
-      continue
-    if i % max(1,(nb_cnfg/10)) == 0:
-      print '\tread config %i' % i
-    data_cnfg = np.zeros((0, T), dtype=np.complex)
-    qn_cnfg = []
-    for p in lookup_p_b:
-      diagram = 'C4+B'
-      path = directory + 'cnfg%i/' % i + 'cnfg%i/' % i + diagram + \
-             '/first_p_%1i%1i%1i/' % (p[0, 0], p[0, 1], p[0, 2])
-      name = diagram + '_uuuu_p%1i%1i%1i.d000.g5' % (p[0, 0], p[0, 1], p[0, 2]) + \
-             '_p%1i%1i%1i.d000.g5' % (p[1, 0], p[1, 1], p[1, 2]) + \
-             '_p%1i%1i%1i.d000.g5' % (p[2, 0], p[2, 1], p[2, 2]) + \
-             '_p%1i%1i%1i.d000.g5' % (p[3, 0], p[3, 1], p[3, 2]) + '.dat'
-    
-      # filename and path
-      filename = os.path.join(path, name)
-      if verbose:
-        print 'Reading data from file:'
-        print '\t\t' + filename
-
-      if cnfg == 0:
-        # set up quantum numbers and reference shape in first iteration
-        ensemble_data = [p[0], np.zeros((3,)), np.asarray(5, dtype=int), \
-                         p[1], np.zeros((3,)), np.asarray(5, dtype=int), \
-                         p[2], np.zeros((3,)), np.asarray(5, dtype=int), \
-                         p[3], np.zeros((3,)), np.asarray(5, dtype=int), name]
-        qn_cnfg.append(ensemble_data)
-        quantum_numbers = np.asarray(qn_cnfg)
-  
-      read_data = np.zeros(T, dtype=np.complex)
-  
-      try:
-        f = open(filename, 'rb')
-      except IOError:
-        print '\tFailed to open %s' % filename
+   
+    # calculate number of configurations
+    nb_cnfg = 0
+    for i in range(sta_cnfg, end_cnfg+1, del_cnfg):
+      if i in missing_configs:
         continue
-      if verbose:
-        print f
-      
-      # actual reading of complex number
-      for t in range(0,T):
-        read_data[t] = complex(struct.unpack('d', f.read(8))[0], \
-                               struct.unpack('d', f.read(8))[0])
-      f.close()
-      data_cnfg = np.vstack((data_cnfg, read_data))
+      nb_cnfg = nb_cnfg + 1
+    if(verbose):
+      print 'number of configurations: %i' % nb_cnfg
   
-    # check if number of operators is consistent between configurations and 
-    # operators are identical
-#    if(cnfg == 0):
-#      shape = data_cnfg.shape
-#      quantum_numbers = qn_cnfg
-#    else:
-#      if(data_cnfg.shape != shape):
-#        print 'Wrong number of operators for cnfg %i' % i
-#        exit(0)
-#      if(quantum_numbers.shape != qn_cnfg.shape):
-#        print 'Wrong operator for cnfg %i' %i
-#        exit(0)
-#      for q,r in zip(quantum_numbers.flatten(), qn_cnfg.flatten()):
-#        if not np.array_equal(q, r):
-#          print 'Wrong operator for cnfg %i' %i
-#          exit(0)
-  
-    data[cnfg] = data_cnfg
-    cnfg = cnfg + 1
-  
-  # convert data to a 3-dim np-array with nb_op x x T x nb_cnfg 
-  data = np.asarray(data)
-  data = np.rollaxis(data, 0, 3)
-  print data.shape
-  print quantum_numbers.shape
+    #TODO: check if all files are there
+    data = [None]*nb_cnfg
+    ensemble_data = []
+    cnfg = 0
+    for i in range(sta_cnfg, end_cnfg+1, del_cnfg):
+      if i in missing_configs:
+        continue
+      if i % max(1,(nb_cnfg/10)) == 0:
+        print '\tread config %i' % i
 
-  print '\tfinished reading'
-  
-  ################################################################################
-  # write data to disc
-  
-  utils.ensure_dir('./readdata')
-  utils.ensure_dir('./readdata/p%1i' % p_cm)
-  utils.ensure_dir('./readdata/p%1i/single' % p_cm)
-  # write every operator seperately
-  for i in range(0, quantum_numbers.shape[0]):
-    path = './readdata/p%1i/single/%s' % \
-            (p_cm, quantum_numbers[i][-1])
-    np.save(path, data[i])
-  
-  # write all operators
-  path = './readdata/p%1i/%s_p%1i' % (p_cm, diagram, p_cm)
-  np.save(path, data)
-  
-  # write all quantum numbers
-  path = './readdata/p%1i/%s_p%1i_quantum_numbers' % (p_cm, diagram, p_cm)
-  np.save(path, quantum_numbers)
-  
-  print '\tfinished writing'
+      data_cnfg = np.zeros((0, T), dtype=np.complex)
 
+      itera, lookup_p = it.tee(lookup_p, 2)
+      for p in itera:
+
+        # filename and path
+        path = directory[d] + 'cnfg%i/' % i + 'cnfg%i/' % i + diagram + \
+               '/first_p_%1i%1i%1i/' % (p[0][0], p[0][1], p[0][2])
+        name = diagram + '_uuuu_p%1i%1i%1i.d000.g5' % (p[0][0], p[0][1], p[0][2]) + \
+               '_p%1i%1i%1i.d000.g5' % (p[1][0], p[1][1], p[1][2]) + \
+               '_p%1i%1i%1i.d000.g5' % (p[2][0], p[2][1], p[2][2]) + \
+               '_p%1i%1i%1i.d000.g5' % (p[3][0], p[3][1], p[3][2]) + '.dat'
+        filename = os.path.join(path, name)
+        if verbose:
+          print 'Reading data from file:'
+          print '\t\t' + filename
+
+        # open file
+        try:
+          f = open(filename, 'rb')
+        except IOError:
+          print '\tFailed to open %s' % filename
+          continue
+        if verbose:
+          print f
+ 
+        # set up quantum numbers and reference shape in first iteration
+        if cnfg == 0:
+          ensemble_data.append([p[0], np.zeros((3,)), np.asarray(5, dtype=int), \
+                           p[1], np.zeros((3,)), np.asarray(5, dtype=int), \
+                           p[2], np.zeros((3,)), np.asarray(5, dtype=int), \
+                           p[3], np.zeros((3,)), np.asarray(5, dtype=int), name])
+    
+        
+        # actual reading of complex number
+        read_data = np.zeros(T, dtype=np.complex)
+        for t in range(0,T):
+          read_data[t] = complex(struct.unpack('d', f.read(8))[0], \
+                                 struct.unpack('d', f.read(8))[0])
+        f.close()
+        data_cnfg = np.vstack((data_cnfg, read_data))
+    
+      # check if number of operators is consistent between configurations and 
+      # operators are identical
+  #    if(cnfg == 0):
+  #      shape = data_cnfg.shape
+  #      quantum_numbers = qn_cnfg
+  #    else:
+  #      if(data_cnfg.shape != shape):
+  #        print 'Wrong number of operators for cnfg %i' % i
+  #        exit(0)
+  #      if(quantum_numbers.shape != qn_cnfg.shape):
+  #        print 'Wrong operator for cnfg %i' %i
+  #        exit(0)
+  #      for q,r in zip(quantum_numbers.flatten(), qn_cnfg.flatten()):
+  #        if not np.array_equal(q, r):
+  #          print 'Wrong operator for cnfg %i' %i
+  #          exit(0)
+    
+      data[cnfg] = data_cnfg
+      cnfg = cnfg + 1
+    
+    quantum_numbers = np.asarray(ensemble_data)
+    # convert data to a 3-dim np-array with nb_op x T x nb_cnfg 
+    data = np.asarray(data)
+    data = np.rollaxis(data, 0, 3)
+
+    print quantum_numbers.shape
+    print data.shape
+  
+    print '\tfinished reading'
+    
+    ################################################################################
+    # write data to disc
+    
+    utils.ensure_dir('./readdata')
+    utils.ensure_dir('./readdata/p%1i' % p_cm)
+    utils.ensure_dir('./readdata/p%1i/single' % p_cm)
+    # write every operator seperately
+    for i in range(0, quantum_numbers.shape[0]):
+      path = './readdata/p%1i/single/%s' % \
+              (p_cm, quantum_numbers[i][-1])
+      np.save(path, data[i])
+    
+    # write all operators
+    path = './readdata/p%1i/%s_p%1i' % (p_cm, diagram, p_cm)
+    np.save(path, data)
+    
+    # write all quantum numbers
+    path = './readdata/p%1i/%s_p%1i_quantum_numbers' % (p_cm, diagram, p_cm)
+    np.save(path, quantum_numbers)
+    
+    print '\tfinished writing\n'
+  
