8027f9dd0ecbd41d71df8078dc4e7764a73ecd31
maowerner <s6mawerner@uni-bonn.de>
2016-05-16 17:26:03 +0200

    rewrote dispersion_relation_pi for new data layout and general momenta

--- analyse_may/dispersion_relation_pi.py
+++ analyse_may/dispersion_relation_pi.py
@@ -15,11 +15,11 @@ import matplotlib.mlab as mlab
 import IOcontraction
 
 import sys
-sys.stdout = open('./dispersion_relation_pi.out', 'w')
+#sys.stdout = open('./dispersion_relation_pi.out', 'w')
 
 # some global variables
 ################################################################################
-p = 0 # momenta to analyse
+momenta = range(0,5) # momenta to analyse
 L = 24 # lattice extend
 pvalue_cut = 0.0 # minimal p-value
 min_fitrange = 2 # minimal fit range
@@ -89,189 +89,86 @@ def weighted_quantile(data, weights, quantile):
 ################################################################################
 ################################################################################
 ################################################################################
-# (2pt) reading data extracted from direct fit
-############ p = 0 #############
-filename = 'bootdata/C2_massfit_p0.npy'
-C2_read0 = np.load(filename)
-filename = 'bootdata/C2_massfit_params_p0.npy'
-C2_params0 = np.load(filename)
-############ p = 1 #############
-filename = 'bootdata/C2_massfit_p1.npy'
-C2_read1 = np.load(filename)
-filename = 'bootdata/C2_massfit_params_p1.npy'
-C2_params1 = np.load(filename)
-############ p = 2 #############
-filename = 'bootdata/C2_massfit_p2.npy'
-C2_read2 = np.load(filename)
-filename = 'bootdata/C2_massfit_params_p2.npy'
-C2_params2 = np.load(filename)
-############ p = 3 #############
-filename = 'bootdata/C2_massfit_p3.npy'
-C2_read3 = np.load(filename)
-filename = 'bootdata/C2_massfit_params_p3.npy'
-C2_params3 = np.load(filename)
 
-# choosing combinations with wanted p-values and fit ranges
+for p in momenta:
+
+  # (2pt) reading data extracted from direct fit
+  filename = 'bootdata/C2_massfit_p%d.npy' % p
+  C2_read = np.load(filename)
+  filename = 'bootdata/C2_massfit_params_p%d.npy' % p
+  C2_params = np.load(filename)
+ 
+  ##############################################################################
+  # choosing combinations with wanted p-values and fit ranges
+  C2_mass, C2_mass_params = \
+           choose_pvalue_cut(C2_read, C2_params, pvalue_cut, min_fitrange)
+
+  # compute weights
+  C2_mass_weight = np.asarray(compute_weight(C2_mass, C2_mass_params))
+
+  # median and std are needed later for the dispersion relation plot. Create 
+  # array to save them
+  if p == momenta[0]:
+    C2_mass_median = np.empty((len(momenta), C2_mass.shape[1]), dtype=float)
+    C2_mass_std = np.empty((len(momenta),1))
+
+  ##############################################################################
+  # writing the 2pt pion mass out and creating a histogram with all data
+  plot_path = './plots/Pion_dispersion.pdf'
+  IOcontraction.ensure_dir(plot_path)
+  pdfplot = PdfPages(plot_path)
+
+  # Creating the histogram
+  for i in range(0, C2_mass.shape[1]):
+    C2_mass_median[p,i] = weighted_quantile(C2_mass[0:-1,i], \
+                                                      C2_mass_weight[0:-1], 0.5)
+  C2_mass_std[p] = np.std(C2_mass_median[p])
+  C2_mass_16quant = weighted_quantile(C2_mass[0:-1,0], \
+                                                     C2_mass_weight[0:-1], 0.16)
+  C2_mass_84quant = weighted_quantile(C2_mass[0:-1,0], \
+                                                     C2_mass_weight[0:-1], 0.84)
+  
+  C2_mass_sys_lo = C2_mass_median[p,0] - C2_mass_16quant
+  C2_mass_sys_hi = C2_mass_84quant   - C2_mass_median[p,0]
+  print 'pion mass for p=%d:' % p
+  print '\tmedian +/- stat + sys - sys'
+  print '\t(%.5e %.1e + %.1e - %.1e)\n' % \
+           (C2_mass_median[p,0], C2_mass_std[p], C2_mass_sys_hi, C2_mass_sys_lo)
+  hist, bins = np.histogram(C2_mass[0:-1,0], 20, \
+                                     weights=C2_mass_weight[0:-1], density=True)
+  width = 0.7 * (bins[1] - bins[0])
+  center = (bins[:-1] + bins[1:]) / 2
+  # Plotting the histogram
+  plt.xlabel(r'$m_{eff}$')
+  plt.ylabel(r'weighted distribution of m_{eff}')
+  plt.title('p=%d' % p)
+  plt.grid(True)
+  x = np.linspace(center[0], center[-1], 1000)
+  plt.plot(x, scipy.stats.norm.pdf(x, loc=C2_mass_median[p,0], 
+           scale=C2_mass_std[p]), 'r-', lw=3, alpha=1, \
+                                                   label='median + stat. error')
+  plt.plot(x, scipy.stats.norm.pdf(x, loc=C2_mass_median[p,0], \
+           scale=0.5*(C2_mass_sys_lo+C2_mass_sys_hi)), 'y-', lw=3, alpha=1, \
+                                                    label='median + sys. error')
+  plt.legend()
+  plt.bar(center, hist, align='center', width=width, alpha=0.7)
+  pdfplot.savefig()
+  plt.clf()
+
 ################################################################################
-C2_mass0, C2_mass_params0 = \
-         choose_pvalue_cut(C2_read0, C2_params0, pvalue_cut, min_fitrange)
-C2_mass1, C2_mass_params1 = \
-         choose_pvalue_cut(C2_read1, C2_params1, pvalue_cut, min_fitrange)
-C2_mass2, C2_mass_params2 = \
-         choose_pvalue_cut(C2_read2, C2_params2, pvalue_cut, min_fitrange)
-C2_mass3, C2_mass_params3 = \
-         choose_pvalue_cut(C2_read3, C2_params3, pvalue_cut, min_fitrange)
-
-# compute weights
-################################################################################
-C2_mass_weight0 = np.asarray(compute_weight(C2_mass0, C2_mass_params0))
-C2_mass_weight1 = np.asarray(compute_weight(C2_mass1, C2_mass_params1))
-C2_mass_weight2 = np.asarray(compute_weight(C2_mass2, C2_mass_params2))
-C2_mass_weight3 = np.asarray(compute_weight(C2_mass3, C2_mass_params3))
-
-# writing the 2pt pion mass out and creating a histogram with all data
-################################################################################
-plot_path = './plots/Pion_dispersion.pdf'
-IOcontraction.ensure_dir(plot_path)
-pdfplot = PdfPages(plot_path)
-################################################################################
-############ p = 0 #############
-C2_mass_median0 = np.empty([C2_mass0.shape[1]], dtype=float)
-for i in range(0, C2_mass0.shape[1]):
-  C2_mass_median0[i] = weighted_quantile(C2_mass0[0:-1,i], C2_mass_weight0[0:-1], 0.5)
-C2_mass_std0 = np.std(C2_mass_median0)
-C2_mass_16quant0 = weighted_quantile(C2_mass0[0:-1,0], C2_mass_weight0[0:-1], 0.16)
-C2_mass_84quant0 = weighted_quantile(C2_mass0[0:-1,0], C2_mass_weight0[0:-1], 0.84)
-
-C2_mass_sys_lo0 = C2_mass_median0[0] - C2_mass_16quant0
-C2_mass_sys_hi0 = C2_mass_84quant0   - C2_mass_median0[0]
-print 'pion mass for p=0:'
-print '\tmedian +/- stat + sys - sys'
-print '\t(%.5e %.1e + %.1e - %.1e)\n' % (C2_mass_median0[0], C2_mass_std0, C2_mass_sys_hi0, C2_mass_sys_lo0)
-hist, bins = np.histogram(C2_mass0[0:-1,0], 20, weights=C2_mass_weight0[0:-1], density=True)
-width = 0.7 * (bins[1] - bins[0])
-center = (bins[:-1] + bins[1:]) / 2
-plt.xlabel(r'$m_{eff}$')
-plt.ylabel(r'weighted distribution of m_{eff}')
-plt.title('p=0')
-plt.grid(True)
-x = np.linspace(center[0], center[-1], 1000)
-plt.plot(x, scipy.stats.norm.pdf(x, loc=C2_mass_median0[0], scale=C2_mass_std0),\
-         'r-', lw=3, alpha=1, label='median + stat. error')
-plt.plot(x, scipy.stats.norm.pdf(x, loc=C2_mass_median0[0], \
-         scale=0.5*(C2_mass_sys_lo0+C2_mass_sys_hi0)),\
-         'y-', lw=3, alpha=1, label='median + sys. error')
-plt.legend()
-plt.bar(center, hist, align='center', width=width, alpha=0.7)
-pdfplot.savefig()
-plt.clf()
-############ p = 1 #############
-C2_mass_median1 = np.empty([C2_mass1.shape[1]], dtype=float)
-for i in range(0, C2_mass1.shape[1]):
-  C2_mass_median1[i] = weighted_quantile(C2_mass1[0:-1,i], C2_mass_weight1[0:-1], 0.5)
-C2_mass_std1 = np.std(C2_mass_median1)
-C2_mass_16quant1 = weighted_quantile(C2_mass1[0:-1,0], C2_mass_weight1[0:-1], 0.16)
-C2_mass_84quant1 = weighted_quantile(C2_mass1[0:-1,0], C2_mass_weight1[0:-1], 0.84)
-
-C2_mass_sys_lo1 = C2_mass_median1[0] - C2_mass_16quant1
-C2_mass_sys_hi1 = C2_mass_84quant1   - C2_mass_median1[0]
-print 'pion mass for p=1:'
-print '\tmedian +/- stat + sys - sys'
-print '\t(%.5e %.1e + %.1e - %.1e)\n' % (C2_mass_median1[0], C2_mass_std1, C2_mass_sys_hi1, C2_mass_sys_lo1)
-hist, bins = np.histogram(C2_mass1[0:-1,0], 20, weights=C2_mass_weight1[0:-1], density=True)
-width = 0.7 * (bins[1] - bins[0])
-center = (bins[:-1] + bins[1:]) / 2
-plt.xlabel(r'$m_{eff}$')
-plt.ylabel(r'weighted distribution of m_{eff}')
-plt.title('p=1')
-plt.grid(True)
-x = np.linspace(center[0], center[-1], 1000)
-plt.plot(x, scipy.stats.norm.pdf(x, loc=C2_mass_median1[0], scale=C2_mass_std1),\
-         'r-', lw=3, alpha=1, label='median + stat. error')
-plt.plot(x, scipy.stats.norm.pdf(x, loc=C2_mass_median1[0], \
-         scale=0.5*(C2_mass_sys_lo1+C2_mass_sys_hi1)),\
-         'y-', lw=3, alpha=1, label='median + sys. error')
-plt.legend()
-plt.bar(center, hist, align='center', width=width, alpha=0.7)
-pdfplot.savefig()
-plt.clf()
-############ p = 2 #############
-C2_mass_median2 = np.empty([C2_mass2.shape[1]], dtype=float)
-for i in range(0, C2_mass2.shape[1]):
-  C2_mass_median2[i] = weighted_quantile(C2_mass2[0:-1,i], C2_mass_weight2[0:-1], 0.5)
-C2_mass_std2 = np.std(C2_mass_median2)
-C2_mass_16quant2 = weighted_quantile(C2_mass2[0:-1,0], C2_mass_weight2[0:-1], 0.16)
-C2_mass_84quant2 = weighted_quantile(C2_mass2[0:-1,0], C2_mass_weight2[0:-1], 0.84)
-
-C2_mass_sys_lo2 = C2_mass_median2[0] - C2_mass_16quant2
-C2_mass_sys_hi2 = C2_mass_84quant2   - C2_mass_median2[0]
-print 'pion mass for p=2:'
-print '\tmedian +/- stat + sys - sys'
-print '\t(%.5e %.1e + %.1e - %.1e)\n' % (C2_mass_median2[0], C2_mass_std2, C2_mass_sys_hi2, C2_mass_sys_lo2)
-hist, bins = np.histogram(C2_mass2[0:-1,0], 20, weights=C2_mass_weight2[0:-1], density=True)
-width = 0.7 * (bins[1] - bins[0])
-center = (bins[:-1] + bins[1:]) / 2
-plt.xlabel(r'$m_{eff}$')
-plt.ylabel(r'weighted distribution of m_{eff}')
-plt.title('p=2')
-plt.grid(True)
-x = np.linspace(center[0], center[-1], 1000)
-plt.plot(x, scipy.stats.norm.pdf(x, loc=C2_mass_median2[0], scale=C2_mass_std2),\
-         'r-', lw=3, alpha=1, label='median + stat. error')
-plt.plot(x, scipy.stats.norm.pdf(x, loc=C2_mass_median2[0], \
-         scale=0.5*(C2_mass_sys_lo2+C2_mass_sys_hi2)),\
-         'y-', lw=3, alpha=1, label='median + sys. error')
-plt.legend()
-plt.bar(center, hist, align='center', width=width, alpha=0.7)
-pdfplot.savefig()
-plt.clf()
-############ p = 3 #############
-C2_mass_median3 = np.empty([C2_mass3.shape[1]], dtype=float)
-for i in range(0, C2_mass3.shape[1]):
-  C2_mass_median3[i] = weighted_quantile(C2_mass3[0:-1,i], C2_mass_weight3[0:-1], 0.5)
-C2_mass_std3 = np.std(C2_mass_median3)
-C2_mass_16quant3 = weighted_quantile(C2_mass3[0:-1,0], C2_mass_weight3[0:-1], 0.16)
-C2_mass_84quant3 = weighted_quantile(C2_mass3[0:-1,0], C2_mass_weight3[0:-1], 0.84)
-
-C2_mass_sys_lo3 = C2_mass_median3[0] - C2_mass_16quant3
-C2_mass_sys_hi3 = C2_mass_84quant3   - C2_mass_median3[0]
-print 'pion mass for p=3:'
-print '\tmedian +/- stat + sys - sys'
-print '\t(%.5e %.1e + %.1e - %.1e)\n' % (C2_mass_median3[0], C2_mass_std3, C2_mass_sys_hi3, C2_mass_sys_lo3)
-hist, bins = np.histogram(C2_mass3[0:-1,0], 20, weights=C2_mass_weight3[0:-1], density=True)
-width = 0.7 * (bins[1] - bins[0])
-center = (bins[:-1] + bins[1:]) / 2
-plt.xlabel(r'$m_{eff}$')
-plt.ylabel(r'weighted distribution of m_{eff}')
-plt.title('p=3')
-plt.grid(True)
-x = np.linspace(center[0], center[-1], 1000)
-plt.plot(x, scipy.stats.norm.pdf(x, loc=C2_mass_median3[0], scale=C2_mass_std3),\
-         'r-', lw=3, alpha=1, label='median + stat. error')
-plt.plot(x, scipy.stats.norm.pdf(x, loc=C2_mass_median3[0], \
-         scale=0.5*(C2_mass_sys_lo3+C2_mass_sys_hi3)),\
-         'y-', lw=3, alpha=1, label='median + sys. error')
-plt.legend()
-plt.bar(center, hist, align='center', width=width, alpha=0.7)
-pdfplot.savefig()
-plt.clf()
-
-
 # Plotting the dispersion relation
 l = -0.05
-u = 0.25
+u = 0.30
 plt.xlim([l, u])
 plt.xlabel(r'$p^2$')
 plt.ylabel(r'$E_{eff}$')
-plt.errorbar(0,                  C2_mass_median0[0], C2_mass_std0, fmt='x' + 'b')
-plt.errorbar((2.*np.pi/24)**2,   C2_mass_median1[0], C2_mass_std1, fmt='x' + 'b')
-plt.errorbar(2*(2.*np.pi/24)**2, C2_mass_median2[0], C2_mass_std2, fmt='x' + 'b')
-plt.errorbar(3*(2.*np.pi/24)**2, C2_mass_median3[0], C2_mass_std3, fmt='x' + 'b')
-
+for p in momenta:
+  plt.errorbar(p*(2.*np.pi/24)**2, C2_mass_median[p,0], C2_mass_std[p], \
+                                                                  fmt='x' + 'b')
 x1 = np.linspace(0, u, 1000)
 y1 = []
 for i in x1:
-  y1.append(np.sqrt(C2_mass_median0[0]**2 + i))
+  y1.append(np.sqrt(C2_mass_median[0,0]**2 + i))
 y1 = np.asarray(y1)
 plt.plot(x1, y1, 'r')
 pdfplot.savefig()
