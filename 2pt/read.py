c6660b23d62d78116ee0815e95a9540da085d4d2
maowerner <s6mawern@uni-bonn.de>
2016-06-20 17:35:57 +0200

    working progress changing 2pt function to get filename rather than os.walk

--- 2pt/read.py
+++ 2pt/read.py
@@ -3,7 +3,10 @@
 import os, glob
 import struct
 
+import h5py
 import numpy as np
+import itertools as it
+import operator
 
 import sys
 #sys.stdout = open('./read.out', 'w')
@@ -14,17 +17,34 @@ import utils
 # parameters ###################################################################
 
 sta_cnfg = 714
-end_cnfg = 1214
+end_cnfg = 715
 del_cnfg = 2
 
 T = 48        # number of timeslices
-p = 0         # momentum
+p_max = 4
+p = range(1)         # momentum
+
+# gamma structure wich shall be averaged. Last entry of gamma must contain all
+# names in LaTeX compatible notation for plot labels
+gamma_i =   [1, 2, 3, \
+             ['\gamma_1', '\gamma_2', '\gamma_3', '\gamma_i']]
+gamma_0i =  [10, 11, 12, \
+             ['\gamma_0\gamma_1', '\gamma_0\gamma_2', '\gamma_0\gamma_3', \
+              '\gamma_0\gamma_i']]
+gamma_50i = [13, 14, 15, \
+             ['\gamma_5\gamma_0\gamma_1', '\gamma_5\gamma_0\gamma_2', \
+              '\gamma_5\gamma_0\gamma_3', '\gamma_5\gamma_0\gamma_i']]
+gamma_5 = [5, ['\gamma_5']]
+
+gammas = [gamma_i, gamma_0i, gamma_50i]
+
+diagrams = ['C20']
 
 verbose = 0
 
 #directory = '/hiskp2/werner/analyse_jan/A40/data/'
 #directory = '/hiskp2/knippsch/Test_rho_2pt/'
-directory = '/hiskp2/knippsch/Rho_Feb2016/'
+directory = '/hiskp2/knippsch/Rho_Jun2016/'
 
 missing_configs = [1282]
 
@@ -45,12 +65,45 @@ def split_to_vector(string):
     i = i+1
   return vector
 
+def scalar_mul(x, y):
+  return sum(it.imap(operator.mul, x, y))
+
+def abs2(x):
+  return scalar_mul(x, x)
+
+def set_lookup_p(p_cm, diagram):
+
+  # create lookup table for all possible 3-momenta that can appear in our 
+  # contractions
+  lookup_p3 = it.ifilter(lambda x: abs2(x) <= p_max, \
+                                 it.product(range(-p_max, p_max+1), repeat=3))
+  
+  lookup_p3, tmp = it.tee(lookup_p3, 2)
+  tmp = list(it.ifilter(lambda x : abs2(x) == p_cm, tmp))
+  if diagram == 'C20':
+    lookup_p = it.ifilter(lambda (x,y): \
+               # if momentum is the desired center of mass momentum and 
+               # respects momentum conservation
+               (tuple(x) == tuple(it.imap(operator.neg, y))) and \
+                                                            (tuple(x) in tmp), \
+               # create lookup table with all possible combinations 
+               # of three 3-momenta 
+                                                 it.product(lookup_p3, repeat=2))
+  else:
+    print 'in set_lookup_p: diagram unknown! Quantum numbers corrupted.'
+  
+  return lookup_p
+
+#def set_lookup_g(gammas, diagram):
+
 ################################################################################
 # reading configurations
 
-def read_ensembles(sta_cnfg, end_cnfg, del_cnfg, p, T, directory, missing_configs, verbose=0):
+def read_ensembles(sta_cnfg, end_cnfg, del_cnfg, diagram, p, T, directory, \
+                                                    missing_configs, verbose=0):
   
-  print 'reading data for p=%i' % p
+  print 'reading data for %s, p=%i' % (diagram, p_cm)
+
   # calculate number of configurations
   nb_cnfg = 0
   for i in range(sta_cnfg, end_cnfg+1, del_cnfg):
@@ -60,163 +113,206 @@ def read_ensembles(sta_cnfg, end_cnfg, del_cnfg, p, T, directory, missing_config
   if(verbose):
     print 'number of configurations: %i' % nb_cnfg
 
+  lookup_p = set_lookup_p(p_cm, diagram)
+  lookup_g = it.product([g for gamma in gammas for g in gamma[:-1]], repeat=2)
 
-  
   #TODO: check if all files are there
   data = [None]*nb_cnfg
   cnfg = 0
    
+#  for i in range(sta_cnfg, end_cnfg+1, del_cnfg):
+#    if i in missing_configs:
+#      continue
+#    if i % (nb_cnfg/10) == 0:
+#      print '\tread config %i' % i
+##    data_cnfg = np.zeros((0, T), dtype=np.complex)
+#    data_cnfg = []
+#    qn_cnfg = []
+#    # only walk through directories containing the configuration number or a 'C'
+#    # (for Correlator)
+#    for root, dirs, files in os.walk(directory, topdown=True):
+#      c = ['cnfg%i' % i, 'C20', 'first']
+#      dirs[:] = [d for d in dirs for cc in c if d.find(cc) !=-1]
+#      # sorted ensures same order of operators for every cnfg
+#      for name in sorted(files):
+#        # running over all subdirectories and searching for files starting with 
+#        # "C20_uu"
+#        if name.startswith('C20_uu'):
+#        # filename and path
+#          filename = os.path.join(root, name)
+#
+#          # getting momentum, displacement and gamma structure from name
+#          split = name.replace('_p', ' ').replace('.d', ' '). \
+#                  replace('.g', ' ').replace('.dat', ' ').split()
+#          ensemble_data = [split_to_vector(split[1]), split_to_vector(split[2]), \
+#                           np.asarray(split[3], dtype=int), \
+#                           split_to_vector(split[4]), split_to_vector(split[5]), \
+#                           np.asarray(split[6], dtype=int), name]
+#  
+#          # ensure p is correct
+#          if not ( (np.dot(ensemble_data[0], ensemble_data[0]) == p) and \
+#                   (np.dot(ensemble_data[3], ensemble_data[3]) == p) ):
+#            continue
+
+  data = []
+
   for i in range(sta_cnfg, end_cnfg+1, del_cnfg):
     if i in missing_configs:
       continue
-    if i % (nb_cnfg/10) == 0:
-      print '\tread config %i' % i
-#    data_cnfg = np.zeros((0, T), dtype=np.complex)
+#    if i % (nb_cnfg/10) == 0:
+#      print '\tread config %i' % i
+
+ 
+    # filename and path
+    filename = directory + 'cnfg%i/' % i + diagram + '_cnfg%i' % i + '.h5'
+#    name = 
+#    filename = os.path.join(path, name)
+    if verbose:
+      print filename
+
+    f = h5py.File(filename, "r")
+
     data_cnfg = []
-    qn_cnfg = []
-    # only walk through directories containing the configuration number or a 'C'
-    # (for Correlator)
-    for root, dirs, files in os.walk(directory, topdown=True):
-      c = ['cnfg%i' % i, 'C20', 'first']
-      dirs[:] = [d for d in dirs for cc in c if d.find(cc) !=-1]
-      # sorted ensures same order of operators for every cnfg
-      for name in sorted(files):
-        # running over all subdirectories and searching for files starting with 
-        # "C20_uu"
-        if name.startswith('C20_uu'):
-        # filename and path
-          filename = os.path.join(root, name)
+    iterator_p, lookup_p = it.tee(lookup_p, 2)
+    for p in iterator_p:
+      iterator_g, lookup_g = it.tee(lookup_g, 2)
+      for g in iterator_g:
 
-          # getting momentum, displacement and gamma structure from name
-          split = name.replace('_p', ' ').replace('.d', ' '). \
-                  replace('.g', ' ').replace('.dat', ' ').split()
-          ensemble_data = [split_to_vector(split[1]), split_to_vector(split[2]), \
-                           np.asarray(split[3], dtype=int), \
-                           split_to_vector(split[4]), split_to_vector(split[5]), \
-                           np.asarray(split[6], dtype=int), name]
-  
-          # ensure p is correct
-          if not ( (np.dot(ensemble_data[0], ensemble_data[0]) == p) and \
-                   (np.dot(ensemble_data[3], ensemble_data[3]) == p) ):
-            continue
-  
-          read_data = np.zeros(T, dtype=np.complex)
+        groupname =u'C20_uu_p200.d000.g3_p-200.d000.g10' 
+        groupname = diagram + '_uu_p%1i%1i%1i.d000.g%i' % \
+                                             (p[0][0], p[0][1], p[0][2], g[0]) \
+                    + '_p%1i%1i%1i.d000.g%i' % (p[1][0], p[1][1], p[1][2], g[1])
 
-          if verbose:
-            print 'Reading data from file:'
-            print '\t\t' + filename
-          try:
-            f = open(filename, 'rb')
-          except IOError:
-            continue
-         
-          # actual reading of complex number
-          for t in range(0,T):
-            read_data[t] = complex(struct.unpack('d', f.read(8))[0], \
-                                   struct.unpack('d', f.read(8))[0])
-          f.close()
-#          data_cnfg = np.vstack((data_cnfg, read_data))
-          data_cnfg.append(read_data)
-          # set up quantum numbers and reference shape in first iteration
-          qn_cnfg.append(ensemble_data)
-  
-    # check if number of operators is consistent between configurations and 
-    # operators are identical
+        if verbose:
+          print groupname
+
+        data_cnfg.append(np.asarray(f[groupname]).view(np.complex))
     data_cnfg = np.asarray(data_cnfg)
-    qn_cnfg = np.asarray(qn_cnfg)
-    if(cnfg == 0):
-      shape = data_cnfg.shape
-      quantum_numbers = qn_cnfg
-    else:
-      if(data_cnfg.shape != shape):
-        print 'Wrong number of operators for cnfg %i' % i
-        exit(0)
-      if(quantum_numbers.shape != qn_cnfg.shape):
-        print 'Wrong operator for cnfg %i' %i
-        exit(0)
-      for q,r in zip(quantum_numbers.flatten(), qn_cnfg.flatten()):
-        if not np.array_equal(q, r):
-          print 'Wrong operator for cnfg %i' %i
-          exit(0)
-  
-    data[cnfg] = data_cnfg
-    cnfg = cnfg + 1
-  
-  # convert data to a 3-dim np-array with nb_op x x T x nb_cnfg 
+    data.append(data_cnfg)
+
   data = np.asarray(data)
-  data = np.rollaxis(data, 0, 3)
   print data.shape
-  
-  print '\tfinished reading'
-  
-  ################################################################################
-  # write data to disc
-  
-  utils.ensure_dir('./readdata')
-  utils.ensure_dir('./readdata/p%1i' % p)
-  utils.ensure_dir('./readdata/p%1i/single' % p)
-  # write every operator seperately
-  for i in range(0, data.shape[0]):
-    path = './readdata/p%1i/single/%s' % \
-            (p, quantum_numbers[i][-1])
-    np.save(path, data[i])
 
+#  
+#          read_data = np.zeros(T, dtype=np.complex)
+#
+#          if verbose:
+#            print 'Reading data from file:'
+#            print '\t\t' + filename
+#          try:
+#            f = open(filename, 'rb')
+#          except IOError:
+#            continue
+#         
+#          # actual reading of complex number
+#          for t in range(0,T):
+#            read_data[t] = complex(struct.unpack('d', f.read(8))[0], \
+#                                   struct.unpack('d', f.read(8))[0])
+#          f.close()
+##          data_cnfg = np.vstack((data_cnfg, read_data))
+#          data_cnfg.append(read_data)
+#          # set up quantum numbers and reference shape in first iteration
+#          qn_cnfg.append(ensemble_data)
+#  
+#    # check if number of operators is consistent between configurations and 
+#    # operators are identical
+#    data_cnfg = np.asarray(data_cnfg)
+#    qn_cnfg = np.asarray(qn_cnfg)
+#    if(cnfg == 0):
+#      shape = data_cnfg.shape
+#      quantum_numbers = qn_cnfg
+#    else:
+#      if(data_cnfg.shape != shape):
+#        print 'Wrong number of operators for cnfg %i' % i
+#        exit(0)
+#      if(quantum_numbers.shape != qn_cnfg.shape):
+#        print 'Wrong operator for cnfg %i' %i
+#        exit(0)
+#      for q,r in zip(quantum_numbers.flatten(), qn_cnfg.flatten()):
+#        if not np.array_equal(q, r):
+#          print 'Wrong operator for cnfg %i' %i
+#          exit(0)
+#  
+#    data[cnfg] = data_cnfg
+#    cnfg = cnfg + 1
+#  
+#  # convert data to a 3-dim np-array with nb_op x x T x nb_cnfg 
+#  data = np.asarray(data)
+#  data = np.rollaxis(data, 0, 3)
+#  print data.shape
+#  
+#  print '\tfinished reading'
+#  
+#  ################################################################################
+#  # write data to disc
+#  
+#  utils.ensure_dir('./readdata')
+#  utils.ensure_dir('./readdata/p%1i' % p)
+#  utils.ensure_dir('./readdata/p%1i/single' % p)
+#  # write every operator seperately
 #  for i in range(0, data.shape[0]):
-#    path = './readdata/p%1i/single/C20_p%i%i%i.d%i%i%i.g%i_p%i%i%i.d%i%i%i.g%i' % \
-#            (p, quantum_numbers[i][0][0], quantum_numbers[i][0][1], 
-#             quantum_numbers[i][0][2], \
-#             quantum_numbers[i][1][0], quantum_numbers[i][1][1], 
-#             quantum_numbers[i][1][2], quantum_numbers[i][2],
-#             quantum_numbers[i][3][0], quantum_numbers[i][3][1], 
-#             quantum_numbers[i][3][2], \
-#             quantum_numbers[i][4][0], quantum_numbers[i][4][1], 
-#             quantum_numbers[i][4][2], quantum_numbers[i][5])
+#    path = './readdata/p%1i/single/%s' % \
+#            (p, quantum_numbers[i][-1])
 #    np.save(path, data[i])
-  
-  # write all operators
-  path = './readdata/p%1i/C20_p%1i' % (p, p)
-  np.save(path, data)
-  
-  # write all quantum numbers
-  path = './readdata/p%1i/C20_p%1i_quantum_numbers' % (p, p)
-  np.save(path, quantum_numbers)
-  
-  print '\tfinished writing'
-  
-  ################################################################################
-  # Code to average operators before bootstrapping. Necessary, if correlators 
-  # are multiplied.
-  
-  #print data.shape
-  #shape = data[0].shape
-  #data = np.reshape(data, (data.shape[0], ) +  data[0].flatten().shape)
-  #
-  #gamma_i = [1, 2, 3]
-  #gamma_0i = [10, 11, 12]
-  #gamma = [gamma_i, gamma_0i, ['g_i', 'g_0g_i']]
-  #
-  #avg = [[None]*(len(gamma)-1)]*(len(gamma)-1)
-  #for i in range(0, len(gamma)-1):
-  #  for j in range(0, len(gamma)-1):
-  #    same_operators = np.zeros((0, data.shape[1]), dtype=np.complex)
-  #    for op in range(0, data.shape[0]):
-  #      if ( (quantum_numbers[op][2] in gamma[i]) and (quantum_numbers[op][5] in gamma[j]) ):
-  #        print same_operators.shape
-  #        print data[op].shape
-  #        same_operators = np.vstack((same_operators, data[op]))
-  #        if(op == 0):
-  #    same_operators = np.reshape(same_operators, (same_operators.shape[0], ) + shape)
-  #    avg[i][j] = np.mean(same_operators, axis=0)
-  #data = np.reshape(data, (data.shape[0], ) + shape)
-  #
-  #avg = np.asarray(avg)
-  #print avg.shape
-  
-  # bootstrap procedure for real and imaginary part seperately
-  # if used, put that into bootstrap.py
-  #avg_real = bootstrap(avg.real, nb_boot)
-  #avg_imag = bootstrap(avg.imag, nb_boot)
-  #print avg.shape
+#
+##  for i in range(0, data.shape[0]):
+##    path = './readdata/p%1i/single/C20_p%i%i%i.d%i%i%i.g%i_p%i%i%i.d%i%i%i.g%i' % \
+##            (p, quantum_numbers[i][0][0], quantum_numbers[i][0][1], 
+##             quantum_numbers[i][0][2], \
+##             quantum_numbers[i][1][0], quantum_numbers[i][1][1], 
+##             quantum_numbers[i][1][2], quantum_numbers[i][2],
+##             quantum_numbers[i][3][0], quantum_numbers[i][3][1], 
+##             quantum_numbers[i][3][2], \
+##             quantum_numbers[i][4][0], quantum_numbers[i][4][1], 
+##             quantum_numbers[i][4][2], quantum_numbers[i][5])
+##    np.save(path, data[i])
+#  
+#  # write all operators
+#  path = './readdata/p%1i/C20_p%1i' % (p, p)
+#  np.save(path, data)
+#  
+#  # write all quantum numbers
+#  path = './readdata/p%1i/C20_p%1i_quantum_numbers' % (p, p)
+#  np.save(path, quantum_numbers)
+#  
+#  print '\tfinished writing'
+#  
+#  ################################################################################
+#  # Code to average operators before bootstrapping. Necessary, if correlators 
+#  # are multiplied.
+#  
+#  #print data.shape
+#  #shape = data[0].shape
+#  #data = np.reshape(data, (data.shape[0], ) +  data[0].flatten().shape)
+#  #
+#  #gamma_i = [1, 2, 3]
+#  #gamma_0i = [10, 11, 12]
+#  #gamma = [gamma_i, gamma_0i, ['g_i', 'g_0g_i']]
+#  #
+#  #avg = [[None]*(len(gamma)-1)]*(len(gamma)-1)
+#  #for i in range(0, len(gamma)-1):
+#  #  for j in range(0, len(gamma)-1):
+#  #    same_operators = np.zeros((0, data.shape[1]), dtype=np.complex)
+#  #    for op in range(0, data.shape[0]):
+#  #      if ( (quantum_numbers[op][2] in gamma[i]) and (quantum_numbers[op][5] in gamma[j]) ):
+#  #        print same_operators.shape
+#  #        print data[op].shape
+#  #        same_operators = np.vstack((same_operators, data[op]))
+#  #        if(op == 0):
+#  #    same_operators = np.reshape(same_operators, (same_operators.shape[0], ) + shape)
+#  #    avg[i][j] = np.mean(same_operators, axis=0)
+#  #data = np.reshape(data, (data.shape[0], ) + shape)
+#  #
+#  #avg = np.asarray(avg)
+#  #print avg.shape
+#  
+#  # bootstrap procedure for real and imaginary part seperately
+#  # if used, put that into bootstrap.py
+#  #avg_real = bootstrap(avg.real, nb_boot)
+#  #avg_imag = bootstrap(avg.imag, nb_boot)
+#  #print avg.shape
 
-for p in range(5):
-  read_ensembles(sta_cnfg, end_cnfg, del_cnfg, p, T, directory, missing_configs, verbose=0)
+for p_cm in p:
+  for diagram in diagrams:
+    read_ensembles(sta_cnfg, end_cnfg, del_cnfg, diagram, p_cm, T, directory, \
+        missing_configs, verbose)
