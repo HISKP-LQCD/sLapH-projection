b235b21558ff50aae012c4bb4ab8b79cd23c0173
maowerner <s6mawern@uni-bonn.de>
2017-02-03 06:39:33 +0100

    finished writing relevant plot functions. Output really messy, but works

--- plot.py
+++ plot.py
@@ -7,6 +7,7 @@ from mpl_toolkits.axes_grid1 import host_subplot
 import mpl_toolkits.axisartist as AA
 import matplotlib.axes as ax
 
+import itertools as it
 import numpy as np
 import pandas as pd
 from pandas import Series, DataFrame
@@ -86,7 +87,7 @@ def plot_gevp(gevp_data, pdfplot):
 ## does not make sence for CMF C2 because there i just one momentum
 #def plot_sep_rows_sep_mom()
 
-def plot_sep_rows_avg_mom(subduced_data, diagram, pdfplot):
+def plot_sep_rows_sum_mom(subduced_data, diagram, pdfplot):
 
   symbol = ['v', '^', '<', '>', 's', 'p', '*', 'h', 'H', 'D', 'd', '8']
 
@@ -117,9 +118,64 @@ def plot_sep_rows_avg_mom(subduced_data, diagram, pdfplot):
 
 
       # prepare parameters for plot design
-      cmap_brg = plt.cm.brg(np.asarray(range(len(rows))) * 256/(len(rows)-1))
+      if len(rows) == 1:
+        cmap_brg=['r']
+      else:
+        cmap_brg = plt.cm.brg(np.asarray(range(len(rows))) * 256/(len(rows)-1))
       shift = 1./3/len(rows)
       label = r'$\mu = %i$' % (counter)
+
+      # plot
+      plt.errorbar(T+shift*counter, mean, std, 
+                       fmt=symbol[counter%len(symbol)], color=cmap_brg[counter], \
+                       label=label, markersize=3, capsize=3, capthick=0.5, \
+                       elinewidth=0.5, markeredgecolor=cmap_brg[counter], \
+                                                                  linewidth='0.0')
+
+
+    # clean up for next plot
+    plt.legend(numpoints=1, loc='best', fontsize=6)
+    pdfplot.savefig()
+    plt.clf()
+
+def plot_avg_rows_sep_mom(subduced_data, diagram, pdfplot):
+
+  symbol = ['v', '^', '<', '>', 's', 'p', '*', 'h', 'H', 'D', 'd', '8']
+
+  subduced_data = mean_and_std(subduced_data)
+
+
+  gevp_index = list(set([(g[0],g[1]) for g in subduced_data.index.values]))
+  for gevp_el_name in gevp_index:
+
+    # prepare plot
+    print 'plotting ', gevp_el_name[0], ' - ', gevp_el_name[1]
+    plt.title(r'Gevp Element ${} - {}$'.format(gevp_el_name[0], gevp_el_name[1]))
+    plt.xlabel(r'$t/a$', fontsize=12)
+    plt.ylabel(r'$%s(t/a)$' % diagram, fontsize=12)
+
+    # prepare data to plot
+    gevp_el = subduced_data.xs(gevp_el_name, level=[0,1])
+    momenta = gevp_el.index.values
+
+    # iterrows() returns a tuple (index, series) where index is a tuple with
+    # strings describing the operators for source and sink and series is the 
+    # data after mean and std where calculated
+    for counter, (p, gevp_el_p) in enumerate(gevp_el.iterrows()):
+
+      T = gevp_el_p.index.levels[1].values
+      mean = gevp_el_p['mean'].values
+      std = gevp_el_p['std'].values
+
+      # prepare parameters for plot design
+      if len(momenta) == 1:
+        cmap_brg=['r']
+      else:
+        cmap_brg = plt.cm.brg(np.asarray(range(len(momenta))) * 256/(len(momenta)-1))
+      shift = 1./3/len(momenta)
+      p_so = p[0]
+      p_si = p[1]
+      label = r'$p_{so} = %s - p_{si} = %s$' % (p_so, p_si)
 
       # plot
       plt.errorbar(T+shift*counter, mean, std, 
@@ -150,17 +206,34 @@ for diagram in ['C2', 'C3', 'C4']:
   subduced_data = subduced_data.sum(level=[0,1,2])
   
   utils.ensure_dir('./plots')
-  plot_path = './plots/%s_p%1i_%s.pdf' % (diagram, p_cm, irrep)
+  plot_path = './plots/%s_sep_rows_sum_mom_p%1i_%s.pdf' % (diagram, p_cm, irrep)
   pdfplot = PdfPages(plot_path)
-  plot_sep_rows_avg_mom(subduced_data, diagram, pdfplot)
+  plot_sep_rows_sum_mom(subduced_data, diagram, pdfplot)
   pdfplot.close()
 
-#path = './readdata/Gevp_p%1i_%s.h5' % (p_cm, irrep)
-#gevp_data = utils.read_hdf5_correlators(path, False)
-#
-#utils.ensure_dir('./plots')
-#plot_path = './plots/Gevp_p%1i_%s.pdf' % (p_cm, irrep)
-#pdfplot = PdfPages(plot_path)
-#plot_gevp(gevp_data, pdfplot)
-#pdfplot.close()
+
+  path = './readdata/%s_p%1i_%s.h5' % (diagram, p_cm, irrep)
+  subduced_data = utils.read_hdf5_correlators(path, False)
+  # discard imaginary part (noise)
+  subduced_data = subduced_data.apply(np.real)
+  # sum over all gamma structures to get the full Dirac operator transforming 
+  # like a row of the desired irrep
+  subduced_data = subduced_data.sum(level=[0,1,2,3,5])
+  # average over rows
+  subduced_data = subduced_data.mean(level=[0,1,3,4])
+  
+  utils.ensure_dir('./plots')
+  plot_path = './plots/%s_avg_rows_sep_mom_p%1i_%s.pdf' % (diagram, p_cm, irrep)
+  pdfplot = PdfPages(plot_path)
+  plot_avg_rows_sep_mom(subduced_data, diagram, pdfplot)
+  pdfplot.close()
+
+path = './readdata/Gevp_p%1i_%s.h5' % (p_cm, irrep)
+gevp_data = utils.read_hdf5_correlators(path, False)
+
+utils.ensure_dir('./plots')
+plot_path = './plots/Gevp_p%1i_%s.pdf' % (p_cm, irrep)
+pdfplot = PdfPages(plot_path)
+plot_gevp(gevp_data, pdfplot)
+pdfplot.close()
 
