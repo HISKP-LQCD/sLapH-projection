709b3d5f4985a37489b3abd53f6391aef4a0ac9a
Markus Werner <s6mawern@uni-bonn.de>
2018-04-06 11:21:40 +0200

    Clean up plot.py

--- src/plot.py
+++ src/plot.py
@@ -13,7 +13,7 @@ import gmpy
 
 import utils
 
-
+################################################################################
 # TODO: Symmetrization and Antisymmetrization. Take negative eigenvalues under
 # time reversal into account
 # TODO: this does not work if I work with the column indices directly rather
@@ -61,7 +61,6 @@ def bootstrap(df, bootstrapsize):
 
     return boot
 
-
 def mean_and_std(df, bootstrapsize):
     """
   Mean and standard deviation over all configurations/bootstrap samples
@@ -93,6 +92,7 @@ def mean_and_std(df, bootstrapsize):
 
     return pd.concat([mean, std], axis=1, keys=['mean', 'std'])
 
+################################################################################
 
 def plot_gevp_el_ax(data, label_template, ax, multiindex=False):
     """
@@ -208,7 +208,7 @@ def plot_gevp_el(data, label_template, multiindex=False):
             markeredgecolor=cmap_brg[counter],
             linewidth='0.0')
 
-def plot_mean_ax(data, ax):
+def plot_mean(data, ax):
 
         # prepare data to plot
         T = data.index.levels[1].astype(int)
@@ -230,33 +230,9 @@ def plot_mean_ax(data, ax):
             markeredgecolor='black',
             linewidth='0.0')
 
-def plot_mean(data):
+################################################################################
 
-        # prepare data to plot
-        T = data.index.levels[1].astype(int)
-        mean = data['mean'].values
-        std = data['std'].values
-
-        # plot
-        plt.errorbar(
-            T,
-            mean,
-            std,
-            fmt='o',
-            color='black',
-            label='mean',
-            markersize=3,
-            capsize=3,
-            capthick=0.75,
-            elinewidth=0.75,
-            markeredgecolor='black',
-            linewidth='0.0')
-
-def group_sum(plotdata,
-                    bootstrapsize,
-                    pdfplot,
-                    logscale=False,
-                    verbose=False):
+def average(plotdata, bootstrapsize, pdfplot, logscale=False, verbose=False):
     """
     Create a multipage plot with a page for every element of the rho gevp
   
@@ -291,39 +267,32 @@ def group_sum(plotdata,
     #linthreshy = plotdata['mean'].iloc[plotdata.loc[:,('mean',0)].nonzero()].abs().min().min()
 
     # create list of gevp elements to loop over
-    plotlabel = list(set([(i[0], i[1], i[2], i[3]) for i in plotdata.index.values]))
+    plotlabel = list(set([(i[0], i[1]) for i in plotdata.index.values]))
     for graphlabel in plotlabel:
 
         if verbose:
-            print '\tplotting ', graphlabel[0], ' - ', graphlabel[1], ', pcm = ', graphlabel[2]
+            print '\tplotting ', graphlabel[0], ' - ', graphlabel[1]
 
         # prepare data to plot
-        graphdata = plotdata.xs(graphlabel, level=['gevp_row', 'gevp_col', 'p_{cm}', '\mu'])
-        # This takes the mean over all operators for the mean and std over 
-        # bootstrapsamples. That is not entirelly correct. The operations should 
-        # be the over way round. good enough for a consistency check.
-        graphdata_mean = graphdata.mean(axis=0)
-
+        graphdata = plotdata.xs(graphlabel, level=['gevp_row', 'gevp_col']).T.squeeze()
 
         # prepare plot
         fig, ax = plt.subplots(1,1)
-        ax.set_title(
-            r'Gevp Element ${}$ - ${}$, $\vec{{P}}_\textnormal{{cm}} = {}$, $\mu = {}$'.format(
-            graphlabel[0], graphlabel[1], graphlabel[2], graphlabel[3]))
+
+        ax.set_title(r'Gevp Element ${}$ - ${}$'.format(
+            graphlabel[0], graphlabel[1]))
         ax.set_xlabel(r'$t/a$', fontsize=12)
         ax.set_ylabel(r'$C(t/a)$', fontsize=12)
         if logscale:
             ax.set_yscale('symlog', linthreshy=linthreshy)
 
         # plot
-        plot_gevp_el_ax(graphdata, r'${}$', ax, multiindex=False)
-
-        plot_mean_ax(graphdata_mean, ax)
+        plot_mean(graphdata, ax)
 
         # clean up for next plot
         ax.legend(numpoints=1, loc='best', fontsize=6)
-
         pdfplot.savefig(fig)
+        plt.close(fig)
 
     return
 
@@ -392,7 +361,7 @@ def pcm_and_mu(plotdata,
         # plot
         plot_gevp_el_ax(graphdata, r'$\vec{{P}}_\textnormal{{cm}} = {}$, $\mu = {}$', ax, multiindex=True)
 
-        plot_mean_ax(graphdata_mean, ax)
+        plot_mean(graphdata_mean, ax)
 
         # clean up for next plot
         ax.legend(numpoints=1, loc='best', fontsize=6)
@@ -401,210 +370,8 @@ def pcm_and_mu(plotdata,
 
     return
 
-def gevp(gevp_data, bootstrapsize, pdfplot, logscale=False, verbose=False):
-    """
-  Create a multipage plot with a page for every element of the rho gevp
 
-  Parameters
-  ----------
-
-  gevp_data : pd.DataFrame
-
-      Table with a row for each gevp element (sorted by gevp column running
-      faster than gevp row) and hierarchical columns for gauge configuration 
-      number and timeslice
-
-  bootstrapsize : int
-
-      The number of bootstrap samples being drawn from `gevp_data`.
-
-  pdfplot : mpl.PdfPages object
-      
-      Plots will be written to the path `pdfplot` was created with.
-
-  See also
-  --------
-
-  utils.create_pdfplot()
-  """
-
-    assert np.all(gevp_data.notnull()), 'Gevp contains null entires'
-    assert gmpy.is_square(len(gevp_data.index)), 'Gevp is not a square matrix'
-
-    gevp_size = gmpy.sqrt(len(gevp_data.index))
-
-
-    gevp_data = mean_and_std(gevp_data, bootstrapsize)
-
-    # abs of smallest positive value
-    #linthreshy = plotdata['mean'][plotdata['mean'] > 0].min().min()
-    # abs of value closest to zero
-    linthreshy = gevp_data['mean'].iloc[gevp_data.loc[:,('mean',0)].nonzero()].abs().min().min()
-
-    # prepare plot
-    fig, axes = plt.subplots(gevp_size, gevp_size, sharex=True, sharey=True)
-
-    for gevp_index, (gevp_el_name, gevp_el_data) in enumerate(gevp_data.iterrows()):
-
-        ax = axes[gevp_index // gevp_size, gevp_index % gevp_size]
-
-        if verbose:
-            print '\tplotting ', gevp_index, gevp_el_name[0], ' - ', gevp_el_name[1]
-
-        # prepare data to plot
-        T = gevp_el_data.index.levels[1].astype(int)
-        mean = gevp_el_data['mean'].values
-        std = gevp_el_data['std'].values
-
-        # prepare parameters for plot design
-#        ax.set_title(r'${} - {}$'.format(gevp_el_name[0], gevp_el_name[1]), fontsize=6)
-#        ax.set_xlabel(r'$t/a$', fontsize=4)
-#        ax.set_ylabel(r'$C(t/a)$', fontsize=4)
-        if logscale:
-            ax.set_yscale('symlog', linthreshy=linthreshy)
-
-        # plot
-        ax.errorbar(
-            T,
-            mean,
-            std,
-            fmt='o',
-            color='black',
-            markersize=3,
-            capsize=3,
-            capthick=0.75,
-            elinewidth=0.75,
-            markeredgecolor='black',
-            linewidth='0.0')
-
-    plt.tight_layout()
-    pdfplot.savefig(fig)
-    plt.close(fig)
-
-    return
-
-
-def avg_row_sum_mom(gevp_data,
-                    bootstrapsize,
-                    pdfplot,
-                    logscale=False,
-                    verbose=False):
-    """
-  Create a multipage plot with a page for every element of the rho gevp
-
-  Parameters
-  ----------
-
-  gevp_data : pd.DataFrame
-
-      Table with a row for each gevp element (sorted by gevp column running
-      faster than gevp row) and hierarchical columns for gauge configuration 
-      number and timeslice
-
-  bootstrapsize : int
-
-      The number of bootstrap samples being drawn from `gevp_data`.
-
-  pdfplot : mpl.PdfPages object
-      
-      Plots will be written to the path `pdfplot` was created with.
-
-  See also
-  --------
-
-  utils.create_pdfplot()
-  """
-
-    gevp_data = mean_and_std(gevp_data, bootstrapsize)
-
-    for gevp_el_name, gevp_el_data in gevp_data.iterrows():
-
-        if verbose:
-            print '\tplotting ', gevp_el_name[0], ' - ', gevp_el_name[1]
-
-        # prepare data to plot
-        T = gevp_el_data.index.levels[1].astype(int)
-        mean = gevp_el_data['mean'].values
-        std = gevp_el_data['std'].values
-
-        # prepare parameters for plot design
-        plt.title(r'Gevp Element ${} - {}$'.format(gevp_el_name[0],
-                                                   gevp_el_name[1]))
-        plt.xlabel(r'$t/a$', fontsize=12)
-        plt.ylabel(r'$C(t/a)$', fontsize=12)
-
-        if logscale:
-            plt.yscale('log')
-
-        # plot
-        plt.errorbar(
-            T,
-            mean,
-            std,
-            fmt='o',
-            color='black',
-            markersize=3,
-            capsize=3,
-            capthick=0.75,
-            elinewidth=0.75,
-            markeredgecolor='black',
-            linewidth='0.0')
-
-        pdfplot.savefig()
-        plt.clf()
-
-    return
-
-
-# does not make sence for CMF C2 because there i just one momentum
-def sep_rows_sep_mom(data,
-                     diagram,
-                     bootstrapsize,
-                     pdfplot,
-                     logscale=False,
-                     verbose=False):
-    #  # discard imaginary part (noise)
-    #  data = data.apply(np.real)
-    # sum over all gamma structures to get the full Dirac operator transforming
-    # like a row of the desired irrep
-    data = data.sum(level=[0, 1, 2, 3, 4, 6])
-    # sum over equivalent momenta
-    data = data.sum(level=[0, 1, 2, 3])
-
-    data = mean_and_std(data, bootstrapsize)
-
-    #  print data.index.map(str, level='p_{cm}')
-    #  data.index.get_level_values('p_{cm}') = data.index.get_level_values('p_{cm}').map(str)
-
-    # create list of gevp elements to loop over
-    gevp_index = list(set([(g[0], g[1]) for g in data.index.values]))
-    for gevp_el_name in gevp_index:
-
-        if verbose:
-            print '\tplotting ', gevp_el_name[0], ' - ', gevp_el_name[1]
-
-        # prepare plot
-        plt.title(r'Gevp Element ${} - {}$'.format(gevp_el_name[0],
-                                                   gevp_el_name[1]))
-        plt.xlabel(r'$t/a$', fontsize=12)
-        plt.ylabel(r'$%s(t/a)$' % diagram, fontsize=12)
-
-        if logscale:
-            plt.yscale('log')
-
-        # prepare data to plot
-        gevp_el = data.xs(gevp_el_name, level=[0, 1])
-
-        # plot
-        plot_gevp_el(gevp_el, r'$p_{{cm}} = {}, \mu = {}$', multiindex=True)
-
-        # clean up for next plot
-        plt.legend(numpoints=1, loc='best', fontsize=6)
-        pdfplot.savefig()
-        plt.clf()
-
-
-def gammas(plotdata, diagram, bootstrapsize, pdfplot, logscale=True, verbose=False):
+def p_and_gammas(plotdata, diagram, bootstrapsize, pdfplot, logscale=True, verbose=False):
     """
     Create a multipage plot with a page for every element of the rho gevp. Each
     page contains one graph for each row of the irrep, summed over all momenta.
@@ -645,19 +412,18 @@ def gammas(plotdata, diagram, bootstrapsize, pdfplot, logscale=True, verbose=Fal
     #linthreshy = plotdata['mean'].iloc[plotdata.loc[:,('mean',0)].nonzero()].abs().min().min()
 
     # create list of gevp elements to loop over
-    plotlabel = list(set([(i[0], i[1], i[2], i[3], i[4]) for i in plotdata.index.values]))
+    plotlabel = list(set([(i[0], i[1], i[2], i[3]) for i in plotdata.index.values]))
     for graphlabel in plotlabel:
 
         if verbose:
             print '\tplotting p_cm = ', graphlabel[2], \
-                ', q = ', graphlabel[3], \
-                ', \mu = ', graphlabel[4]
+                ', \mu = ', graphlabel[3]
 
         # prepare data to plot
         graphdata = plotdata.xs(graphlabel, level=['gevp_row', 'gevp_col', 'p_{cm}', '\mu'])
         # prepare plot
-        plt.title(r'Gevp Element ${}$ - ${}$, $\vec{{P}}_\textnormal{{cm}} = {}$, $\vec{{q}}_\textnormal{{rel}} = {}$, $\mu = {}$'.format(
-            graphlabel[0], graphlabel[1], graphlabel[2], graphlabel[3], graphlabel[4]))
+        plt.title(r'Gevp Element ${}$ - ${}$, $\vec{{P}}_\textnormal{{cm}} = {}$, $\mu = {}$'.format(
+            graphlabel[0], graphlabel[1], graphlabel[2], graphlabel[3]))
         plt.xlabel(r'$t/a$', fontsize=12)
         plt.ylabel(r'$%s(t/a)$' % diagram, fontsize=12)
 
@@ -672,32 +438,18 @@ def gammas(plotdata, diagram, bootstrapsize, pdfplot, logscale=True, verbose=Fal
         pdfplot.savefig()
         plt.clf()
 
-
-def avg_rows_sep_mom(data,
-                     diagram,
-                     bootstrapsize,
-                     pdfplot,
-                     logscale=False,
-                     verbose=False):
+def gevp(gevp_data, bootstrapsize, pdfplot, logscale=False, verbose=False):
     """
-  Create a multipage plot with a page for every element of the rho gevp. Each
-  page contains one graph for each momentum, averaged over all rows of the 
-  irrep. 
+  Create a multipage plot with a page for every element of the rho gevp
 
   Parameters
   ----------
 
-  data : pd.DataFrame
+  gevp_data : pd.DataFrame
 
-      Table with physical quantum numbers as rows 'gevp_row' x 'gevp_col' x \
-      '\mu' x 'p_{so}' x '\gamma_{so}' x 'p_{si}' x '\gamma_{si}' and columns
-      'cnfg' x 'T'.
-      Contains the linear combinations of correlation functions transforming
-      like what the parameters qn_irrep was created with, i.e. the subduced 
-      data
-
-  diagram : string
-      The diagram as it will appear in the plot labels.
+      Table with a row for each gevp element (sorted by gevp column running
+      faster than gevp row) and hierarchical columns for gauge configuration 
+      number and timeslice
 
   bootstrapsize : int
 
@@ -713,44 +465,47 @@ def avg_rows_sep_mom(data,
   utils.create_pdfplot()
   """
 
-    # discard imaginary part (noise)
-    data = data.apply(np.real)
-    # sum over all gamma structures to get the full Dirac operator transforming
-    # like a row of the desired irrep
-    data = data.sum(level=[0, 1, 2, 3, 4, 6])
-    # sum over equivalent momenta
-    data = data.sum(level=[0, 1, 2, 3])
-    # mean over equivalent rows
-    data = data.mean(level=[0, 1, 2])
+    assert np.all(gevp_data.notnull()), 'Gevp contains null entires'
+    gevp_data = mean_and_std(gevp_data, bootstrapsize)
 
-    data = mean_and_std(data, bootstrapsize)
+    gevp_rows = gevp_data.index.get_level_values(level="gevp_row").unique()
+    gevp_cols = gevp_data.index.get_level_values(level="gevp_col").unique()
+    assert len(gevp_rows) == len(gevp_cols), 'Gevp is not a square matrix'
+    gevp_size = len(gevp_rows)
 
-    gevp_index = list(set([(g[0], g[1]) for g in data.index.values]))
-    for gevp_el_name in gevp_index:
+    # abs of smallest positive value
+    #linthreshy = plotdata['mean'][plotdata['mean'] > 0].min().min()
+    # abs of value closest to zero
+    linthreshy = 10e2 * gevp_data['mean'].iloc[gevp_data.loc[:,('mean',0)].nonzero()].abs().min().min()
 
-        if verbose:
-            print 'plotting ', gevp_el_name[0], ' - ', gevp_el_name[1]
+    # prepare plot
+    fig, axes = plt.subplots(gevp_size, gevp_size, dpi=gevp_size*80, sharex=True, sharey=True)
 
-        # prepare plot
-        plt.title(r'Gevp Element ${} - {}$'.format(gevp_el_name[0],
-                                                   gevp_el_name[1]))
-        plt.xlabel(r'$t/a$', fontsize=12)
-        plt.ylabel(r'$%s(t/a)$' % diagram, fontsize=12)
+    for r,row in enumerate(gevp_rows):
+        for c, col in enumerate(gevp_cols):
 
-        if logscale:
-            plt.yscale('log')
+            graphdata = gevp_data.xs((row,col), level=("gevp_row", "gevp_col"))
+            ax = axes[r, c]
 
-        # prepare data to plot
-        gevp_el = data.xs(gevp_el_name, level=[0, 1])
+            if verbose:
+                print '\tplotting ', row, ' - ', col
 
-        # plot
-        plot_gevp_el(gevp_el, r'$p_{{cm}} = {}$')
+            # prepare parameters for plot design
+#            ax.set_title(r'${} - {}$'.format(row, col), fontsize=6)
+#            ax.set_xlabel(r'$t/a$', fontsize=4)
+#            ax.set_ylabel(r'$C(t/a)$', fontsize=4)
+            if logscale:
+                ax.set_yscale('symlog', linthreshy=linthreshy)
 
-        # clean up for next plot
-        plt.legend(numpoints=1, loc='best', fontsize=6)
-        pdfplot.savefig()
-        plt.clf()
+            plot_gevp_el_ax(graphdata, r'$\vec{{P}}_\textnormal{{cm}} = {}$, $\mu = {}$', ax, multiindex=True)
 
+    plt.tight_layout()
+    pdfplot.savefig(fig)
+    plt.close(fig)
+
+    return
+
+################################################################################
 
 def quick_view(data, bootstrapsize, pdfplot, logscale=False, verbose=False):
     # TODO: pass data as list
