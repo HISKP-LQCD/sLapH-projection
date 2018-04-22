94bfd1fc31ab5a353c1df5d90977968b131bfc3a
Markus Werner <s6mawern@uni-bonn.de>
2018-04-22 21:11:30 +0200

    Change read_sc() to new format and read beta

--- src/projection_interface_maple.py
+++ src/projection_interface_maple.py
@@ -52,18 +52,17 @@ def read_sc_2(p_cm_vecs, path, verbose=True, j=1):
     df = pd.read_csv(name, sep="\t", dtype=str)
     df.rename(columns=lambda x: x.strip(), inplace=True)
 
+    df['mult'] = 1
     df['p_{cm}'] = [p_cm_vec] * len(df)
     del df['name']
-    df.rename(columns={'alpha' : '\mu'}, inplace=True)
-    del df['beta']
-    df['mult'] = 1
-    df = df.set_index(['p_{cm}', 'Irrep', '\mu', 'mult'])
+    df.rename(columns={'alpha' : r'\mu', 'beta' : r'\beta'}, inplace=True)
+    df = df.set_index(['Irrep', 'mult', 'p_{cm}', '\mu', r'\beta', ])
 
-    df['coefficient'] = df['coefficient'].apply(aeval)
     df['p^1'] = df['p^1'].apply(literal_eval).apply(str)
     df['p^2'] = df['p^2'].apply(literal_eval).apply(str)
     df['q'] = df['q'].apply(literal_eval).apply(str)
     df.rename(columns={'p^1' : 'p^{0}', 'p^2' : 'p^{1}'}, inplace=True)
+
 
     # Rho cannot appear in S-wave pipi scattering because of angular momentum 
     # conservation.
@@ -83,8 +82,48 @@ def read_sc_2(p_cm_vecs, path, verbose=True, j=1):
     df['M^{0}'] = 0
     df['M^{1}'] = 0
 
+    df['coefficient'] = df['coefficient'].str.replace('^', '**').apply(aeval)
+
     if verbose >= 2:
       print 'Two-particle projection coefficients for {}'.format(p_cm_vec)
+      print df, '\n'
+
+    subduction_coefficients = pd.concat([subduction_coefficients, df])
+
+  return subduction_coefficients.sort_index()
+
+
+def read_sc(p_cm_vecs, path, verbose=True, j=1):
+  """
+  """
+
+  subduction_coefficients = DataFrame()
+
+  for p_cm_vec in p_cm_vecs:
+    name = path +'/' + 'J{0}-P{1}-operators.txt'.format(\
+           j, "".join([str(p) for p in eval(p_cm_vec)]))
+
+    if not os.path.exists(name):
+      print 'Warning: Could not find {}'.format(name)
+      continue
+
+    df = pd.read_csv(name, sep="\t", dtype=str)
+    df.rename(columns=lambda x: x.strip(), inplace=True)
+
+    df['mult'] = 1
+    df['p_{cm}'] = [p_cm_vec] * len(df)
+    df.rename(columns=
+        {'alpha' : r'\mu', 'beta' : r'\beta', 'M' : 'M^{0}'}, inplace=True)
+
+    df['p^{0}'] = [p_cm_vec] * len(df)
+    df['J^{0}'] = j
+    df['M^{0}'] = df['M^{0}'].apply(int)
+    df['coefficient'] = df['coefficient'].str.replace('^', '**').apply(aeval)
+
+    df = df.set_index(['Irrep', 'mult', 'p_{cm}', '\mu', r'\beta'])
+
+    if verbose >= 2:
+      print 'One-particle projection coefficients for {}'.format(p_cm_vec)
       print df, '\n'
 
     subduction_coefficients = pd.concat([subduction_coefficients, df])
@@ -98,70 +137,70 @@ def read_sc_2(p_cm_vecs, path, verbose=True, j=1):
 # We get an additional column \vec{q} that works similar to the row of the 
 # irrep. Either the maple script has to find unique operators, or an additional
 # unique function must be used here. I prefer the former.
-def read_sc(p_cm_vecs, path, verbose=True, j=1):
-  """
-  Read subduction coefficients from SO(3) to irreducible representations of 
-  appropriate little group of rotational symmetry for lattice in a reference 
-  frame moving with momentum p_cm \in list_p_cm
-
-  Parameters
-  ----------
-    p_cm_vecs : list
-        Center of mass momentum of the lattice. Used to specify the appropriate
-        little group of rotational symmetry. Contains integer 3-vectors
-    path : string
-        Path to files with subduction coefficients
-
-  Returns
-  -------
-    df : pd.DataFrame
-        Contains subduction coefficients for going from continuum to discete
-        space. 
-        Has columns Irrep, mult, J, M, coefficient, p, \mu and unnamed 
-        indices
-
-  Note
-  ----
-    Filename of subduction coefficients hardcoded. Expected to be 
-    "J%d-P%1i%1i%1i-operators.txt"
-    # "lattice-basis_J%d_P%1i%1i%1i_Msum.dataframe"
-  """
-
-  subduction_coefficients = DataFrame()
-
-  for p_cm_vec in p_cm_vecs:
-
-#    name = path +'/' + 'lattice-basis_J{0}_P{1}_Msum.dataframe'.format(\
+#def read_sc(p_cm_vecs, path, verbose=True, j=1):
+#  """
+#  Read subduction coefficients from SO(3) to irreducible representations of 
+#  appropriate little group of rotational symmetry for lattice in a reference 
+#  frame moving with momentum p_cm \in list_p_cm
+#
+#  Parameters
+#  ----------
+#    p_cm_vecs : list
+#        Center of mass momentum of the lattice. Used to specify the appropriate
+#        little group of rotational symmetry. Contains integer 3-vectors
+#    path : string
+#        Path to files with subduction coefficients
+#
+#  Returns
+#  -------
+#    df : pd.DataFrame
+#        Contains subduction coefficients for going from continuum to discete
+#        space. 
+#        Has columns Irrep, mult, J, M, coefficient, p, \mu and unnamed 
+#        indices
+#
+#  Note
+#  ----
+#    Filename of subduction coefficients hardcoded. Expected to be 
+#    "J%d-P%1i%1i%1i-operators.txt"
+#    # "lattice-basis_J%d_P%1i%1i%1i_Msum.dataframe"
+#  """
+#
+#  subduction_coefficients = DataFrame()
+#
+#  for p_cm_vec in p_cm_vecs:
+#
+##    name = path +'/' + 'lattice-basis_J{0}_P{1}_Msum.dataframe'.format(\
+##           j, "".join([str(p) for p in eval(p_cm_vec)]))
+#    name = path +'/' + 'J{0}-P{1}-operators.txt'.format(\
 #           j, "".join([str(p) for p in eval(p_cm_vec)]))
-    name = path +'/' + 'J{0}-P{1}-operators.txt'.format(\
-           j, "".join([str(p) for p in eval(p_cm_vec)]))
-
-    if not os.path.exists(name):
-      print 'Warning: Could not find {}'.format(name)
-      continue
-    df = pd.read_csv(name, delim_whitespace=True, dtype=str) 
-
-    df = pd.merge(df.ix[:,2:].stack().reset_index(level=1), df.ix[:,:2], 
-                  left_index=True, 
-                  right_index=True)
-
-    # Munging of column names
-    df.columns = ['M^{0}', 'coefficient', 'Irrep', '\mu']
-    df['mult'] = 1
-    df['p_{cm}'] = [p_cm_vec] * len(df)
-    df['coefficient'] = df['coefficient'].apply(aeval)
-    df['M^{0}'] = df['M^{0}'].apply(int)
-    df = df.set_index(['p_{cm}', 'Irrep', '\mu', 'mult'])
-    df['p^{0}'] = [p_cm_vec] * len(df)
-    df['J^{0}'] = j
-    df = df[['p^{0}','J^{0}','M^{0}','coefficient']]
-
-    if verbose >= 2:
-      print 'One-particle projection coefficients for {}'.format(p_cm_vec)
-      print df, '\n'
-
-    subduction_coefficients = pd.concat([subduction_coefficients, df])
-
-  return subduction_coefficients.sort_index()
+#
+#    if not os.path.exists(name):
+#      print 'Warning: Could not find {}'.format(name)
+#      continue
+#    df = pd.read_csv(name, delim_whitespace=True, dtype=str) 
+#
+#    df = pd.merge(df.ix[:,2:].stack().reset_index(level=1), df.ix[:,:2], 
+#                  left_index=True, 
+#                  right_index=True)
+#
+#    # Munging of column names
+#    df.columns = ['M^{0}', 'coefficient', 'Irrep', '\mu']
+#    df['mult'] = 1
+#    df['p_{cm}'] = [p_cm_vec] * len(df)
+#    df['coefficient'] = df['coefficient'].str.replace('^', '**').apply(aeval)
+#    df['M^{0}'] = df['M^{0}'].apply(int)
+#    df = df.set_index(['p_{cm}', 'Irrep', '\mu', 'mult'])
+#    df['p^{0}'] = [p_cm_vec] * len(df)
+#    df['J^{0}'] = j
+#    df = df[['p^{0}','J^{0}','M^{0}','coefficient']]
+#
+#    if verbose >= 2:
+#      print 'One-particle projection coefficients for {}'.format(p_cm_vec)
+#      print df, '\n'
+#
+#    subduction_coefficients = pd.concat([subduction_coefficients, df])
+#
+#  return subduction_coefficients.sort_index()
 
 
