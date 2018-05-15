480fc28d3b3b95dfda1774afeb42d2d2ea0f32a0
Markus Werner <s6mawern@uni-bonn.de>
2018-05-15 14:59:39 +0200

    Default values for gevp parameters

--- src/infile_handler.py
+++ src/infile_handler.py
@@ -3,33 +3,34 @@ import ConfigParser
 
 def get_parameters():
 
-  ##############################################################################
-  # Argument parsing ########################################################### 
+    ##############################################################################
+    # Argument parsing ###########################################################
 
-  parser = argparse.ArgumentParser()
-  # parse the name of the infile and load its contents into the parser
-  parser.add_argument("infile", help="name of input file")
-  # verbosity is also parsed
-  parser.add_argument("-v", "--verbose", action="count", default=0, \
-                                                 help="increase output verbosity")
-  # TODO: Only allow certain options, specify standard behavior, etc.
-  parser.add_argument("-b", "--basis", choices=['cartesian', 'cyclic', \
-                        'cyclic-christian', 'dudek', 'marcus-cov', 'marcus-con', 'test'], \
-                        default='marcus-cov',
-                        help="continuum basis to be used in the program")
-  
-  args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    # parse the name of the infile and load its contents into the parser
+    parser.add_argument("infile", help="name of input file")
+    # verbosity is also parsed
+    parser.add_argument("-v", "--verbose", action="count", default=0, \
+                                                   help="increase output verbosity")
+    # TODO: Only allow certain options, specify standard behavior, etc.
+    parser.add_argument("-b", "--basis", choices=['cartesian', 'cyclic', \
+                          'cyclic-christian', 'dudek', 'marcus-cov', 'marcus-con', 'test'], \
+                          default='marcus-cov',
+                          help="continuum basis to be used in the program")
 
-  ##############################################################################
-  # Reading infile #############################################################
-  
-  config = ConfigParser.SafeConfigParser(
-          {'use old data format' : False, 'beta' : 1, 'list of q' : None, 'list of p_cm' : None})
-  
-  if(config.read(args.infile) == []):
-    print "Error! Could not open infile: ", args.infile
-    exit(-1)
+    args = parser.parse_args()
 
-  return args, config
-  
- 
+    ##############################################################################
+    # Reading infile #############################################################
+
+    config = ConfigParser.SafeConfigParser(
+            {'list of p_cm' : None,
+                'Dirac structure' : 'gamma_i',
+                'list of q' : None,
+                'beta' : '1'})
+
+    if(config.read(args.infile) == []):
+        print "Error! Could not open infile: ", args.infile
+        exit(-1)
+
+    return args, config
