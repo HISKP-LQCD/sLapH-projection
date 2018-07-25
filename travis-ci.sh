8b1f19eb556d916d3d1714e67a1316c50b329820
Markus Werner <s6mawern@uni-bonn.de>
2018-07-25 15:54:01 +0200

    Move setUp into Testing

--- travis-ci.sh
+++ travis-ci.sh
@@ -25,22 +25,6 @@ sudo apt-get update
 sudo apt-get install -y "${ubuntu_packages[@]}"
 
 ##########################################################################################
-# Setup                                                                                  #
-##########################################################################################
-
-
-# Set up output folder
-mkdir -p "$outdir/integration/3_gevp-data"
-cp "$sourcedir/tests/integration/p"*".ini" "$outdir/integration/3_gevp-data/"
-
-# Set up input data
-mkdir -p "$HOME/Data"
-pushd "$sourcedir"
-python travis-ci_setup.py
-popd
-tar -C "$HOME/Data/" -xf "$HOME/Data/A40.24-cnfg0714.tar"
-
-##########################################################################################
 # Run tests                                                                              #
 ##########################################################################################
 
